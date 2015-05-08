389c561a6fb1bb2bd88b8eff01e1a50ea1493b2e
versluisant <rackipava@gmail.com>
2015-05-08 15:05:17 +0300

    sdf
    sdf

---
+++ WebApplication2/Scripts/app/js/controllers.js
@@ -0,0 +1,15 @@
+﻿var app = angular.module('webStoreApp', []);
+
+app.controller('categoriesCtrl', function ($scope) {
+    $scope.categories = [
+      { 'name': 'books' },
+      { 'name': 'phones' },
+      { 'name': 'hardware' }
+    ];
+});
+
+app.controller('inventoryCtrl', ['$scope', '$http', function ($scope, $http) {
+    var result = $http.get('/Json/inventory.json').success(function (data) {
+        $scope.phones = data;
+    })
+}]);
